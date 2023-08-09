b04c1b205739d85bf7968a210b9211b510968d37
Aldin <aldinmuratovic24@gmail.com>
2023-08-09 10:23:44 +0200

    Dedalus: fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,15 +2,25 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from "./components/home/home.component";
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { SharedModule } from "./shared/shared.module";
+import { StoreModule } from "@ngrx/store";
+import { sharedReducer } from "./shared/store/reducers/shared.reducer";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        SharedModule,
+        StoreModule.forRoot({
+          shared: sharedReducer
+        }),
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        NavbarComponent
       ],
     }).compileComponents();
   });
@@ -19,5 +29,11 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(HomeComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'DedalusTask'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('DedalusTask');
   });
 });
--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { StoreModule } from "@ngrx/store";
+import { sharedReducer } from "../../shared/store/reducers/shared.reducer";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +10,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [
+        StoreModule.forRoot({ sharedReducer })
+      ]
     })
     .compileComponents();
 
